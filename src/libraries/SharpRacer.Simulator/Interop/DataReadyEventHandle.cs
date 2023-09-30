3bf332ccbbdec53426cd252fee94406dfa75a57d
Eric Smith <33182789+ericmqt@users.noreply.github.com>
2023-09-30 14:51:42 -0400

    Add SharpRacer.Simulator project, add data-ready native event handle

---
+++ src/libraries/SharpRacer.Simulator/Interop/DataReadyEventHandle.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Win32.SafeHandles;
+using Windows.Win32;
+
+namespace SharpRacer.Simulator.Interop;
+internal static class DataReadyEventHandle
+{
+    internal const string EventName = "Local\\IRSDKDataValidEvent";
+
+    internal static SafeWaitHandle CreateSafeWaitHandle()
+    {
+        return PInvoke.CreateEventAsSafeWaitHandle(
+            lpEventAttributes: null,
+            bManualReset: true,
+            bInitialState: false,
+            lpName: EventName);
+    }
+}
