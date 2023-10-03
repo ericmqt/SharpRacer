5ef8640679e1bdb1b23d61dc7de45eca9ce53806
Eric Smith <33182789+ericmqt@users.noreply.github.com>
2023-10-03 13:52:26 -0400

    Sketch out initial simulator commands

---
+++ src/libraries/SharpRacer.Simulator/Interop/BroadcastMessage.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+
+namespace SharpRacer.Simulator.Interop;
+internal static class BroadcastMessage
+{
+    private static readonly HWND HWND_BROADCAST = new(0xFFFF);
+    private static readonly uint IRSDK_BROADCASTMSG = PInvoke.RegisterWindowMessage("IRSDK_BROADCASTMSG");
+
+    public static void Send(SimulatorCommandId commandId, int param1, int param2)
+    {
+        PInvoke.SendNotifyMessage(
+            HWND_BROADCAST,
+            IRSDK_BROADCASTMSG,
+            CreateWParam(commandId, param1),
+            param2);
+    }
+
+    public static void Send(SimulatorCommandId commandId, int param1, float param2)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void Send(SimulatorCommandId commandId, int param1, int param2, int param3)
+    {
+        PInvoke.SendNotifyMessage(
+            HWND_BROADCAST,
+            IRSDK_BROADCASTMSG,
+            CreateWParam(commandId, param1),
+            CreateLParam(param2, param3));
+    }
+
+    private static void SendCommand(WPARAM wParam, LPARAM lParam)
+    {
+        PInvoke.SendNotifyMessage(HWND_BROADCAST, IRSDK_BROADCASTMSG, wParam, lParam);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static LPARAM CreateLParam(int low, int high)
+    {
+        return (int)(((ushort)low) | (((uint)high) << 16));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static WPARAM CreateWParam(SimulatorCommandId commandId, int high)
+    {
+        return ((ushort)commandId) | (((uint)high) << 16);
+    }
+}
---
+++ src/libraries/SharpRacer/Simulator/SimulatorCommandId.cs
@@ -0,0 +1,19 @@
+﻿namespace SharpRacer.Simulator;
+public enum SimulatorCommandId : ushort
+{
+    CameraSwitchPosition = 0,
+    CameraSwitchNumber = 1,
+    CameraSetState = 2,
+    ReplaySetPlaySpeed = 3,
+    ReplaySetPlayPosition = 4,
+    ReplaySearch = 5,
+    ReplaySetState = 6,
+    ReloadTextures = 7,
+    ChatCommand = 8,
+    PitCommand = 9,
+    TelemetryCommand = 10,
+    ForceFeedbackCommand = 11,
+    ReplaySearchSessionTime = 12,
+    VideoCapture = 13,
+    Unused = 14
+}
