85568dcd3aca7df439f3d6fb53b6e9984a6a768a
Eric Smith <33182789+ericmqt@users.noreply.github.com>
2023-11-22 12:14:09 -0500

    Create source generator project and debug target app

---
+++ src/generators/SharpRacer.SourceGenerators/TelemetryVariablesGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace SharpRacer.SourceGenerators;
+
+[Generator(LanguageNames.CSharp)]
+public sealed class TelemetryVariablesGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/generators/sourcegendebug/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace SourceGeneratorDebugApp;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
