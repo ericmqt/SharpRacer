fb9af840906ea54fe11176e1d35ab29455874aff
Eric Smith <33182789+ericmqt@users.noreply.github.com>
2023-12-10 10:44:48 -0500

    Add unit tests for VariableInfoFileName and VariableOptionsFileName

---
+++ src/tests/SharpRacer.SourceGenerators.UnitTests/TelemetryVariables/InputModels/VariableInfoFileNameTests.cs
@@ -0,0 +1,80 @@
+﻿using SharpRacer.SourceGenerators.Testing;
+
+namespace SharpRacer.SourceGenerators.TelemetryVariables.InputModels;
+public class VariableInfoFileNameTests
+{
+    [Fact]
+    public void Ctor_Test()
+    {
+        var fileName = new VariableInfoFileName("Foo.bar");
+
+        string fileNameValue = fileName;
+
+        Assert.Equal(fileNameValue, fileName, StringComparer.Ordinal);
+    }
+
+    [Fact]
+    public void Ctor_ThrowOnNullOrEmptyFileNameTest()
+    {
+        Assert.Throws<ArgumentException>(() => new VariableInfoFileName(null!));
+        Assert.Throws<ArgumentException>(() => new VariableInfoFileName(string.Empty));
+    }
+
+    [Fact]
+    public void IsMatch_Test()
+    {
+        var matchingFile = new AdditionalTextFile("Foo.bar", "Hello, world!");
+        var matchingFileWithDirectory = new AdditionalTextFile("src/tests/Foo.bar", "Hello, world!");
+        var nonMatchingFile = new AdditionalTextFile("Bar.baz", "Goodbye, world!");
+        var nonMatchingFileWithDirectory = new AdditionalTextFile("src/tests/Bar.baz", "Goodbye, world!");
+
+        var fileName = new VariableInfoFileName("Foo.bar");
+
+        Assert.True(fileName.IsMatch(matchingFile));
+        Assert.True(fileName.IsMatch(matchingFileWithDirectory));
+        Assert.False(fileName.IsMatch(nonMatchingFile));
+        Assert.False(fileName.IsMatch(nonMatchingFileWithDirectory));
+    }
+
+    [Fact]
+    public void Equals_Test()
+    {
+        var fileName1 = new VariableInfoFileName("Foo.bar");
+        var fileName2 = new VariableInfoFileName("Foo.bar");
+
+        Assert.True(fileName1.Equals(fileName2));
+        Assert.True(fileName1 == fileName2);
+        Assert.False(fileName1 != fileName2);
+        Assert.Equal(fileName1.GetHashCode(), fileName2.GetHashCode());
+    }
+
+    [Fact]
+    public void Equals_DefaultValueTest()
+    {
+        var fileName1 = new VariableInfoFileName("Foo.bar");
+
+        Assert.False(fileName1.Equals(default));
+        Assert.False(fileName1 == default);
+        Assert.True(fileName1 != default);
+        Assert.NotEqual(fileName1.GetHashCode(), default(VariableInfoFileName).GetHashCode());
+    }
+
+    [Fact]
+    public void Equals_ObjectTest()
+    {
+        var fileName1 = new VariableInfoFileName("Foo.bar");
+        object fileName2 = new VariableInfoFileName("Foo.bar");
+        object nonFileNameValue = 56;
+
+        Assert.True(fileName1.Equals(fileName2));
+        Assert.False(fileName1.Equals(nonFileNameValue));
+    }
+
+    [Fact]
+    public void DefaultValueImplicitStringOperatorReturnsEmptyTest()
+    {
+        VariableInfoFileName fileName = default;
+
+        Assert.Equal(string.Empty, fileName);
+    }
+}
---
+++ src/tests/SharpRacer.SourceGenerators.UnitTests/TelemetryVariables/InputModels/VariableOptionsFileNameTests.cs
@@ -0,0 +1,80 @@
+﻿using SharpRacer.SourceGenerators.Testing;
+
+namespace SharpRacer.SourceGenerators.TelemetryVariables.InputModels;
+public class VariableOptionsFileNameTests
+{
+    [Fact]
+    public void Ctor_Test()
+    {
+        var fileName = new VariableOptionsFileName("Foo.bar");
+
+        string fileNameValue = fileName;
+
+        Assert.Equal(fileNameValue, fileName, StringComparer.Ordinal);
+    }
+
+    [Fact]
+    public void Ctor_ThrowOnNullOrEmptyFileNameTest()
+    {
+        Assert.Throws<ArgumentException>(() => new VariableOptionsFileName(null!));
+        Assert.Throws<ArgumentException>(() => new VariableOptionsFileName(string.Empty));
+    }
+
+    [Fact]
+    public void IsMatch_Test()
+    {
+        var matchingFile = new AdditionalTextFile("Foo.bar", "Hello, world!");
+        var matchingFileWithDirectory = new AdditionalTextFile("src/tests/Foo.bar", "Hello, world!");
+        var nonMatchingFile = new AdditionalTextFile("Bar.baz", "Goodbye, world!");
+        var nonMatchingFileWithDirectory = new AdditionalTextFile("src/tests/Bar.baz", "Goodbye, world!");
+
+        var fileName = new VariableOptionsFileName("Foo.bar");
+
+        Assert.True(fileName.IsMatch(matchingFile));
+        Assert.True(fileName.IsMatch(matchingFileWithDirectory));
+        Assert.False(fileName.IsMatch(nonMatchingFile));
+        Assert.False(fileName.IsMatch(nonMatchingFileWithDirectory));
+    }
+
+    [Fact]
+    public void Equals_Test()
+    {
+        var fileName1 = new VariableOptionsFileName("Foo.bar");
+        var fileName2 = new VariableOptionsFileName("Foo.bar");
+
+        Assert.True(fileName1.Equals(fileName2));
+        Assert.True(fileName1 == fileName2);
+        Assert.False(fileName1 != fileName2);
+        Assert.Equal(fileName1.GetHashCode(), fileName2.GetHashCode());
+    }
+
+    [Fact]
+    public void Equals_DefaultValueTest()
+    {
+        var fileName1 = new VariableOptionsFileName("Foo.bar");
+
+        Assert.False(fileName1.Equals(default));
+        Assert.False(fileName1 == default);
+        Assert.True(fileName1 != default);
+        Assert.NotEqual(fileName1.GetHashCode(), default(VariableOptionsFileName).GetHashCode());
+    }
+
+    [Fact]
+    public void Equals_ObjectTest()
+    {
+        var fileName1 = new VariableOptionsFileName("Foo.bar");
+        object fileName2 = new VariableOptionsFileName("Foo.bar");
+        object nonFileNameValue = 56;
+
+        Assert.True(fileName1.Equals(fileName2));
+        Assert.False(fileName1.Equals(nonFileNameValue));
+    }
+
+    [Fact]
+    public void DefaultValueImplicitStringOperatorReturnsEmptyTest()
+    {
+        VariableOptionsFileName fileName = default;
+
+        Assert.Equal(string.Empty, fileName);
+    }
+}
