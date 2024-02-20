3de8a7a2617902bf858833e79dbc4eba21ac7a25
Eric Smith <33182789+ericmqt@users.noreply.github.com>
2024-02-20 09:01:49 -0500

    Add initial SimulatorDataReader implementation to support telemetry variables tool, update SDK type listings in README

--- src/libraries/SharpRacer/README.md
+++ src/libraries/SharpRacer/README.md
@@ -2,8 +2,27 @@
 
 ### SharpRacer and iRacing SDK Types
 
+#### Structures
+
 | iRacing SDK | SharpRacer |
 | ----------- | ---------- |
+| irsdk_diskSubHeader | DiskSubHeader |
 | irsdk_header | DataFileHeader |
 | irsdk_varBuf | DataBufferHeader |
 | irsdk_varHeader | DataVariableHeader |
+
+#### Enumerations
+
+| iRacing SDK | SharpRacer |
+| ----------- | ---------- |
+| irsdk_CameraState | CameraState |
+| irsdk_CarLeftRight | CarLeftRight |
+| irsdk_EngineWarnings | EngineWarnings |
+| irsdk_Flags | RacingFlags |
+| irsdk_PaceFlags | PaceRacingFlags |
+| irsdk_PaceMode | PaceMode |
+| irsdk_PitSvFlags | PitServiceOptions |
+| irsdk_PitSvStatus | PitServiceStatus |
+| irsdk_SessionState | SessionState |
+| irsdk_TrkLoc | TrackLocationType |
+| irsdk_TrkSurf | TrackSurfaceType |
---
+++ src/libraries/SharpRacer/SimulatorDataReader.cs
@@ -0,0 +1,63 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using SharpRacer.Interop;
+
+namespace SharpRacer;
+public readonly ref struct SimulatorDataReader
+{
+    private readonly ReadOnlySpan<byte> _data;
+
+    public SimulatorDataReader(ISimulatorConnection connection)
+    {
+        if (connection is null)
+        {
+            throw new ArgumentNullException(nameof(connection));
+        }
+
+        _data = connection.Data;
+    }
+
+    public SimulatorDataReader(ReadOnlySpan<byte> data)
+    {
+        _data = data;
+    }
+
+    public readonly ReadOnlySpan<byte> Data => _data;
+
+    public DataVariableHeader[] ReadDataVariableHeaders()
+    {
+        var count = MemoryMarshal.Read<int>(_data.Slice(DataFileHeader.FieldOffsets.VariableCount, sizeof(int)));
+        var offset = MemoryMarshal.Read<int>(_data.Slice(DataFileHeader.FieldOffsets.VariableHeaderOffset, sizeof(int)));
+
+        var headers = new DataVariableHeader[count];
+
+        var headersBlob = _data.Slice(offset, count * DataVariableHeader.Size);
+
+        // Copy the source data into our header array reinterpreted as a span of bytes
+        headersBlob.CopyTo(MemoryMarshal.AsBytes<DataVariableHeader>(headers));
+
+        return headers;
+    }
+
+    public DataFileHeader ReadHeader()
+    {
+        return MemoryMarshal.Read<DataFileHeader>(_data[..DataFileHeader.Size]);
+    }
+
+    public string ReadSessionInfo()
+    {
+        var offset = MemoryMarshal.Read<int>(_data.Slice(DataFileHeader.FieldOffsets.SessionInfoOffset, sizeof(int)));
+        var length = MemoryMarshal.Read<int>(_data.Slice(DataFileHeader.FieldOffsets.SessionInfoLength, sizeof(int)));
+        var span = _data.Slice(offset, length);
+
+        // SessionInfo encoding is ISO-8859-1, not UTF8. This only really matters for tracks with non-ASCII characters in their name.
+        return Encoding.Latin1.GetString(span);
+    }
+
+    public int ReadSessionInfoVersion()
+    {
+        var slice = _data.Slice(DataFileHeader.FieldOffsets.SessionInfoVersion, sizeof(int));
+
+        return MemoryMarshal.Read<int>(slice);
+    }
+}
