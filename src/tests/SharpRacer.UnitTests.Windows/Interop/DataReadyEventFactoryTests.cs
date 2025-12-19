76cae8c6f4053fa464b960a543b33d8ef7baba2e
Eric Smith <33182789+ericmqt@users.noreply.github.com>
2025-12-19 16:55:21 -0500

    Add DataReadyEventFactory tests

---
+++ src/tests/SharpRacer.UnitTests.Windows/Interop/DataReadyEventFactoryTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpRacer.Interop;
+
+public class DataReadyEventFactoryTests
+{
+    [Fact]
+    public void CreateAutoResetEvent_Test()
+    {
+        var factory = new DataReadyEventFactory();
+
+        var dataReadyEvent = factory.CreateAutoResetEvent(false);
+
+        Assert.NotNull(dataReadyEvent);
+        Assert.False(dataReadyEvent.SafeWaitHandle.IsClosed);
+        Assert.False(dataReadyEvent.SafeWaitHandle.IsInvalid);
+    }
+
+    [Fact]
+    public void CreateSafeWaitHandle_Test()
+    {
+        var handle = DataReadyEventFactory.CreateSafeWaitHandle();
+
+        Assert.False(handle.IsClosed);
+        Assert.False(handle.IsInvalid);
+    }
+}
