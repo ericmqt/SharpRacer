9fe4df5e93193005201ec28013937268127cd88d
Eric Smith <33182789+ericmqt@users.noreply.github.com>
2025-12-28 16:40:12 -0500

    Update package: DotNext.Threading, DotNext.Unsafe

--- src/tests/SharpRacer.Testing/IO/FakeDataFileMappedMemory.cs
+++ src/tests/SharpRacer.Testing/IO/FakeDataFileMappedMemory.cs
@@ -1,4 +1,5 @@
-﻿using DotNext.IO.MemoryMappedFiles;
+﻿using System.Runtime.InteropServices;
+using DotNext.IO.MemoryMappedFiles;
 using DotNext.Runtime.InteropServices;
 
 namespace SharpRacer.IO;
@@ -10,8 +11,12 @@ internal sealed class FakeDataFileMappedMemory : IMappedMemory
     public FakeDataFileMappedMemory(Memory<byte> memory)
     {
         _memory = memory;
+
+        // Unused but initialization required
+        Buffer = null!;
     }
 
+    public SafeBuffer Buffer { get; }
     public int Length => _memory.Length;
     public Pointer<byte> Pointer { get; }
     public Span<byte> Span => _memory.Span;
