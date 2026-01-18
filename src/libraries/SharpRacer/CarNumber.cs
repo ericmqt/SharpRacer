d481d6e9c8d548fbced9848df7a414b9225d0f90
Eric Smith <33182789+ericmqt@users.noreply.github.com>
2026-01-18 13:51:41 -0500

    Add CarNumber struct, encapsulating irsdk_padCarNum from the iRacing SDK

---
+++ src/libraries/SharpRacer/CarNumber.cs
@@ -0,0 +1,282 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace SharpRacer;
+
+/// <summary>
+/// Represents a car number.
+/// </summary>
+/// <remarks>
+/// The car number is encoded as an integer that accounts for leading zeroes, allowing car numbers such as "7" and "07" to be easily
+/// distinguished.
+/// 
+/// See irsdk_padCarNum in the iRacing SDK.
+/// </remarks>
+public readonly struct CarNumber : IEquatable<CarNumber>, IParsable<CarNumber>, ISpanParsable<CarNumber>
+{
+    internal const int LengthFactor = 1000;
+
+    /// <summary>
+    /// Initializes a new <see cref="CarNumber"/> value from its encoded integer representation.
+    /// </summary>
+    /// <param name="rawValue">The encoded integer representation of the car number.</param>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// <paramref name="rawValue"/> is less than zero or greater than 3999.
+    /// </exception>
+    public CarNumber(int rawValue)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(rawValue);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(rawValue, 3999);
+
+        Base = rawValue - ((rawValue / LengthFactor) * LengthFactor);
+        var nBaseDigits = GetNonZeroDigitCount(Base);
+        var nLeadingZeroes = Math.Max((rawValue / LengthFactor) - nBaseDigits, 0);
+
+        Length = nLeadingZeroes + nBaseDigits;
+        Value = nLeadingZeroes > 0 ? Base + (Length * LengthFactor) : Base;
+    }
+
+    /// <summary>
+    /// Gets the integer representation of the car number.
+    /// </summary>
+    internal readonly int Base { get; }
+
+    /// <summary>
+    /// Gets the length of the car number when formatted as a string, including any leading zeroes.
+    /// </summary>
+    internal readonly int Length { get; }
+
+    /// <summary>
+    /// Gets an encoded integer representation of the car number, accounting for any leading zeroes.
+    /// </summary>
+    public readonly int Value { get; }
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj)
+    {
+        return obj is CarNumber number && Equals(number);
+    }
+
+    /// <inheritdoc />
+    public bool Equals(CarNumber other)
+    {
+        // The other properties are calculated from Value so there is no need to compare them all, just Value.
+
+        return Value == other.Value;
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Value);
+    }
+
+    /// <summary>
+    /// Returns the string representation of the car number, including any leading zeroes.
+    /// </summary>
+    /// <returns>The car number as a string.</returns>
+    public override string ToString()
+    {
+        return Base.ToString($"D{Length}");
+    }
+
+    /// <summary>
+    /// Parses a string into a <see cref="CarNumber"/> value.
+    /// </summary>
+    /// <param name="value">The string to parse.</param>
+    /// <param name="provider">An object that provides culture-specific formatting information about <paramref name="value"/>.</param>
+    /// <returns>The result of parsing <paramref name="value"/>.</returns>
+    /// <exception cref="ArgumentException">
+    /// <paramref name="value"/> is an empty string or <see langword="null"/>.
+    /// 
+    /// -OR-
+    /// 
+    /// The length of <paramref name="value"/> is greater than three.
+    /// </exception>
+    /// <exception cref="FormatException">
+    /// <paramref name="value"/> is not in the correct format.
+    /// </exception>
+    public static CarNumber Parse(string value, IFormatProvider? provider = null)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(value);
+
+        if (value.Length > 3)
+        {
+            throw new ArgumentException($"'{nameof(value)}' must have a length less than or equal to 3.", nameof(value));
+        }
+
+        return Parse(value.AsSpan(), provider);
+    }
+
+    /// <summary>
+    /// Parses a span of characters into a <see cref="CarNumber"/> value.
+    /// </summary>
+    /// <param name="valueSpan">The span of characters to parse.</param>
+    /// <param name="provider">An object that provides culture-specific formatting information about <paramref name="valueSpan"/>.</param>
+    /// <returns>The result of parsing <paramref name="valueSpan"/>.</returns>
+    /// <exception cref="ArgumentException">
+    /// <paramref name="valueSpan"/> is empty.
+    /// 
+    /// -OR-
+    /// 
+    /// The length of <paramref name="valueSpan"/> is greater than three.
+    /// </exception>
+    /// <exception cref="FormatException">
+    /// The string represented by <paramref name="valueSpan"/> is not in the correct format.
+    /// </exception>
+    public static CarNumber Parse(ReadOnlySpan<char> valueSpan, IFormatProvider? provider = null)
+    {
+        if (valueSpan.Length == 0)
+        {
+            throw new ArgumentException($"'{nameof(valueSpan)}' cannot be empty.", nameof(valueSpan));
+        }
+
+        if (valueSpan.Length > 3)
+        {
+            throw new ArgumentException($"'{nameof(valueSpan)}' must have a length less than or equal to 3.", nameof(valueSpan));
+        }
+
+        int leadingZeroCount = GetLeadingZeroCount(valueSpan);
+
+        if (leadingZeroCount == valueSpan.Length)
+        {
+            return new CarNumber(leadingZeroCount * LengthFactor);
+        }
+
+        var carNumberBaseSpan = valueSpan[leadingZeroCount..];
+
+        if (!int.TryParse(carNumberBaseSpan, out var carNumberBase))
+        {
+            throw new FormatException($"Unable to parse non-leading-zero part '{carNumberBaseSpan}' as an integer.");
+        }
+
+        return new CarNumber(carNumberBase + (valueSpan.Length * LengthFactor));
+    }
+
+    /// <summary>
+    /// Attempts to parse a string into a <see cref="CarNumber"/> value.
+    /// </summary>
+    /// <param name="value">The string to parse.</param>
+    /// <param name="result">
+    /// When this method returns, contains the result of successfully parsing <paramref name="value"/> or the default value on failure.
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/> if <paramref name="value"/> was successfully parsed, otherwise <see langword="false"/>.
+    /// </returns>
+    public static bool TryParse([NotNullWhen(true)] string? value, [MaybeNullWhen(false)] out CarNumber result)
+    {
+        return TryParse(value.AsSpan(), null, out result);
+    }
+
+    /// <summary>
+    /// Attempts to parse a string into a <see cref="CarNumber"/> value.
+    /// </summary>
+    /// <param name="value">The string to parse.</param>
+    /// <param name="provider">An object that provides culture-specific formatting information about <paramref name="value"/>.</param>
+    /// <param name="result">
+    /// When this method returns, contains the result of successfully parsing <paramref name="value"/> or the default value on failure.
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/> if <paramref name="value"/> was successfully parsed, otherwise <see langword="false"/>.
+    /// </returns>
+    public static bool TryParse([NotNullWhen(true)] string? value, IFormatProvider? provider, [MaybeNullWhen(false)] out CarNumber result)
+    {
+        return TryParse(value.AsSpan(), provider, out result);
+    }
+
+    /// <summary>
+    /// Attempts to parse a span of characters into a <see cref="CarNumber"/> value.
+    /// </summary>
+    /// <param name="valueSpan">The span of characters to parse.</param>
+    /// <param name="result">
+    /// When this method returns, contains the result of successfully parsing <paramref name="valueSpan"/> or the default value on failure.
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/> if <paramref name="valueSpan"/> was successfully parsed, otherwise <see langword="false"/>.
+    /// </returns>
+    public static bool TryParse(ReadOnlySpan<char> valueSpan, [MaybeNullWhen(false)] out CarNumber result)
+    {
+        return TryParse(valueSpan, null, out result);
+    }
+
+    /// <summary>
+    /// Attempts to parse a span of characters into a <see cref="CarNumber"/> value.
+    /// </summary>
+    /// <param name="valueSpan">The span of characters to parse.</param>
+    /// <param name="provider">An object that provides culture-specific formatting information about <paramref name="valueSpan"/>.</param>
+    /// <param name="result">
+    /// When this method returns, contains the result of successfully parsing <paramref name="valueSpan"/> or the default value on failure.
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/> if <paramref name="valueSpan"/> was successfully parsed, otherwise <see langword="false"/>.
+    /// </returns>
+    public static bool TryParse(ReadOnlySpan<char> valueSpan, IFormatProvider? provider, [MaybeNullWhen(false)] out CarNumber result)
+    {
+        result = default;
+
+        if (valueSpan.Length == 0 || valueSpan.Length > 3)
+        {
+            return false;
+        }
+
+        int leadingZeroCount = GetLeadingZeroCount(valueSpan);
+
+        if (leadingZeroCount == valueSpan.Length)
+        {
+            result = new CarNumber(leadingZeroCount * LengthFactor);
+            return true;
+        }
+
+        var carNumberBaseSpan = valueSpan[leadingZeroCount..];
+
+        if (!int.TryParse(carNumberBaseSpan, out var carNumberBase))
+        {
+            return false;
+        }
+
+        result = new CarNumber(carNumberBase + (valueSpan.Length * LengthFactor));
+        return true;
+    }
+
+    internal static int GetLeadingZeroCount(ReadOnlySpan<char> span)
+    {
+        int leadingZeroCount = 0;
+
+        for (int i = 0; i < span.Length; i++)
+        {
+            if (span[i] == '0')
+            {
+                leadingZeroCount++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return leadingZeroCount;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static int GetNonZeroDigitCount(int baseCarNumber)
+    {
+        return baseCarNumber switch
+        {
+            > 99 => 3,
+            > 9 => 2,
+            _ => 1
+        };
+    }
+
+    /// <inheritdoc />
+    public static bool operator ==(CarNumber left, CarNumber right)
+    {
+        return left.Equals(right);
+    }
+
+    /// <inheritdoc />
+    public static bool operator !=(CarNumber left, CarNumber right)
+    {
+        return !(left == right);
+    }
+}
---
+++ src/libraries/tests/SharpRacer.UnitTests/CarNumberTests.cs
@@ -0,0 +1,302 @@
+﻿namespace SharpRacer;
+
+using SharpRacer.Extensions.Xunit;
+using ExpectedCarNumberValues = (int ExpectedValue, int ExpectedBase, int ExpectedLength, string ExpectedString);
+
+public class CarNumberTests
+{
+    public static TheoryData<int, ExpectedCarNumberValues> ConstructorTestData { get; } = GetCtorTestData();
+    public static TheoryData<CarNumber, CarNumber> EqualityTestData { get; } = GetEqualityTestData();
+    public static TheoryData<CarNumber, CarNumber> InequalityTestData { get; } = GetInequalityTestData();
+
+    [Theory]
+    [MemberData(nameof(ConstructorTestData))]
+    public void Ctor_Test(int rawValue, ExpectedCarNumberValues expected)
+    {
+        var carNumber = new CarNumber(rawValue);
+
+        Assert.Equal(expected.ExpectedValue, carNumber.Value);
+        Assert.Equal(expected.ExpectedBase, carNumber.Base);
+        Assert.Equal(expected.ExpectedLength, carNumber.Length);
+        Assert.Equal(expected.ExpectedString, carNumber.ToString());
+    }
+
+    [Fact]
+    public void Ctor_ThrowIfRawValueIsNegativeTest()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new CarNumber(-1));
+    }
+
+    [Fact]
+    public void Ctor_ThrowIfRawValueIsGreaterThanMaxTest()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new CarNumber(4000));
+    }
+
+    [Theory]
+    [MemberData(nameof(EqualityTestData))]
+    public void Equals_Test(CarNumber left, CarNumber right)
+    {
+        EquatableStructAssert.Equal(left, right);
+    }
+
+    [Theory]
+    [MemberData(nameof(InequalityTestData))]
+    public void Equals_InequalityTest(CarNumber left, CarNumber right)
+    {
+        EquatableStructAssert.NotEqual(left, right);
+    }
+
+    [Fact]
+    public void Equals_ObjectInequalityTest()
+    {
+        var carNumber = new CarNumber(12);
+        var other = new object();
+
+        EquatableStructAssert.ObjectEqualsMethod(false, carNumber, other);
+    }
+
+    [Fact]
+    public void GetLeadingZeroCount_SpanTest()
+    {
+        const string s0 = "10023456";
+        const string s1 = "01020304";
+        const string s2 = "001230456";
+        const string s3 = "00010002";
+
+        Assert.Equal(0, CarNumber.GetLeadingZeroCount(s0));
+        Assert.Equal(1, CarNumber.GetLeadingZeroCount(s1));
+        Assert.Equal(2, CarNumber.GetLeadingZeroCount(s2));
+        Assert.Equal(3, CarNumber.GetLeadingZeroCount(s3));
+    }
+
+    [Fact]
+    public void GetNonZeroDigitCount_Test()
+    {
+        Assert.Equal(1, CarNumber.GetNonZeroDigitCount(1));
+        Assert.Equal(2, CarNumber.GetNonZeroDigitCount(10));
+        Assert.Equal(3, CarNumber.GetNonZeroDigitCount(111));
+    }
+
+    [Fact]
+    public void Parse_AllZeroesTest()
+    {
+        const string carNumberString = "000";
+        var carNumber = CarNumber.Parse(carNumberString);
+
+        Assert.Equal(0, carNumber.Base);
+        Assert.Equal(3, carNumber.Length);
+        Assert.Equal(3000, carNumber.Value);
+        Assert.Equal(carNumberString, carNumber.ToString());
+    }
+
+    [Fact]
+    public void Parse_NoLeadingZeroesTest()
+    {
+        const string carNumberString = "22";
+        var carNumber = CarNumber.Parse(carNumberString);
+
+        Assert.Equal(22, carNumber.Base);
+        Assert.Equal(2, carNumber.Length);
+        Assert.Equal(22, carNumber.Value);
+        Assert.Equal(carNumberString, carNumber.ToString());
+    }
+
+    [Fact]
+    public void Parse_WithLeadingZeroesTest()
+    {
+        const string carNumberString = "007";
+        var carNumber = CarNumber.Parse(carNumberString);
+
+        Assert.Equal(7, carNumber.Base);
+        Assert.Equal(3, carNumber.Length);
+        Assert.Equal(3007, carNumber.Value);
+        Assert.Equal(carNumberString, carNumber.ToString());
+    }
+
+    [Fact]
+    public void Parse_ThrowOnNonDigitCharacterTest()
+    {
+        Assert.Throws<FormatException>(() => CarNumber.Parse("00X"));
+    }
+
+    [Fact]
+    public void ParseSpan_ThrowIfEmptyTest()
+    {
+        Assert.Throws<ArgumentException>(() => CarNumber.Parse(Span<char>.Empty));
+    }
+
+    [Fact]
+    public void ParseSpan_ThrowIfLengthExceedsMaxTest()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            ReadOnlySpan<char> inputSpan = "1234";
+
+            CarNumber.Parse(inputSpan);
+        });
+    }
+
+    [Fact]
+    public void ParseString_ThrowIfNullOrEmptyTest()
+    {
+        Assert.Throws<ArgumentException>(() => CarNumber.Parse(string.Empty));
+        Assert.Throws<ArgumentNullException>(() => CarNumber.Parse(null!));
+    }
+
+    [Fact]
+    public void ParseString_ThrowIfLengthExceedsMaxTest()
+    {
+        Assert.Throws<ArgumentException>(() => CarNumber.Parse("1234"));
+    }
+
+    [Fact]
+    public void TryParse_AllZeroesTest()
+    {
+        const string carNumberString = "000";
+        Assert.True(CarNumber.TryParse(carNumberString, null, out var carNumber));
+
+        Assert.Equal(0, carNumber.Base);
+        Assert.Equal(3, carNumber.Length);
+        Assert.Equal(3000, carNumber.Value);
+        Assert.Equal(carNumberString, carNumber.ToString());
+    }
+
+    [Fact]
+    public void TryParse_NoLeadingZeroesTest()
+    {
+        const string carNumberString = "22";
+        Assert.True(CarNumber.TryParse(carNumberString, null, out var carNumber));
+
+        Assert.Equal(22, carNumber.Base);
+        Assert.Equal(2, carNumber.Length);
+        Assert.Equal(22, carNumber.Value);
+        Assert.Equal(carNumberString, carNumber.ToString());
+    }
+
+    [Fact]
+    public void TryParse_WithLeadingZeroesTest()
+    {
+        const string carNumberString = "007";
+        Assert.True(CarNumber.TryParse(carNumberString, null, out var carNumber));
+
+        Assert.Equal(7, carNumber.Base);
+        Assert.Equal(3, carNumber.Length);
+        Assert.Equal(3007, carNumber.Value);
+        Assert.Equal(carNumberString, carNumber.ToString());
+    }
+
+    [Fact]
+    public void TryParse_SpanTest()
+    {
+        ReadOnlySpan<char> carNumberSpan = "007";
+        Assert.True(CarNumber.TryParse(carNumberSpan, out var carNumber));
+
+        Assert.Equal(7, carNumber.Base);
+        Assert.Equal(3, carNumber.Length);
+        Assert.Equal(3007, carNumber.Value);
+        Assert.Equal(carNumberSpan, carNumber.ToString());
+    }
+
+    [Fact]
+    public void TryParseSpan_ReturnFalseIfStringExceedsMaxLengthTest()
+    {
+        ReadOnlySpan<char> inputSpan = "1234";
+
+        Assert.False(CarNumber.TryParse(inputSpan, null, out var result));
+        Assert.Equal(default, result);
+    }
+
+    [Fact]
+    public void TryParseString_ReturnFalseIfNullOrEmptyStringTest()
+    {
+        string? value = null;
+        Assert.False(CarNumber.TryParse(value, out var result));
+        Assert.Equal(default, result);
+
+        Assert.False(CarNumber.TryParse(string.Empty, out result));
+        Assert.Equal(default, result);
+    }
+
+    [Fact]
+    public void TryParseString_ReturnFalseIfStringExceedsMaxLengthTest()
+    {
+        Assert.False(CarNumber.TryParse("1234", null, out var result));
+        Assert.Equal(default, result);
+    }
+
+    [Fact]
+    public void TryParseString_ReturnFalseOnNonDigitCharacterTest()
+    {
+        Assert.False(CarNumber.TryParse("00X", null, out _));
+    }
+
+    private static TheoryData<int, ExpectedCarNumberValues> GetCtorTestData()
+    {
+        return new TheoryData<int, ExpectedCarNumberValues>()
+        {
+            { 7, (ExpectedValue: 7, ExpectedBase: 7, ExpectedLength: 1, ExpectedString: "7") },
+            { 1007, (ExpectedValue: 7, ExpectedBase: 7, ExpectedLength: 1, ExpectedString: "7") },
+            { 2007, (ExpectedValue: 2007, ExpectedBase: 7, ExpectedLength: 2, ExpectedString: "07") },
+            { 3007, (ExpectedValue: 3007, ExpectedBase: 7, ExpectedLength: 3, ExpectedString: "007") },
+
+            { 0, (ExpectedValue: 0, ExpectedBase: 0, ExpectedLength: 1, ExpectedString: "0") },
+            { 1000, (ExpectedValue: 0, ExpectedBase: 0, ExpectedLength: 1, ExpectedString: "0") },
+            { 2000, (ExpectedValue: 2000, ExpectedBase: 0, ExpectedLength: 2, ExpectedString: "00") },
+            { 3000, (ExpectedValue: 3000, ExpectedBase: 0, ExpectedLength: 3, ExpectedString: "000") },
+
+            { 22, (ExpectedValue: 22, ExpectedBase: 22, ExpectedLength: 2, ExpectedString: "22") },
+            { 1022, (ExpectedValue: 22, ExpectedBase: 22, ExpectedLength: 2, ExpectedString: "22") },
+            { 2022, (ExpectedValue: 22, ExpectedBase: 22, ExpectedLength: 2, ExpectedString: "22") },
+            { 3022, (ExpectedValue: 3022, ExpectedBase: 22, ExpectedLength: 3, ExpectedString: "022") },
+
+            { 333, (ExpectedValue: 333, ExpectedBase: 333, ExpectedLength: 3, ExpectedString: "333") },
+            { 1333, (ExpectedValue: 333, ExpectedBase: 333, ExpectedLength: 3, ExpectedString: "333") },
+            { 2333, (ExpectedValue: 333, ExpectedBase: 333, ExpectedLength: 3, ExpectedString: "333") },
+            { 3333, (ExpectedValue: 333, ExpectedBase: 333, ExpectedLength: 3, ExpectedString: "333") },
+        };
+    }
+
+    private static TheoryData<CarNumber, CarNumber> GetEqualityTestData()
+    {
+        return new TheoryData<CarNumber, CarNumber>()
+        {
+            { new(0), new(0) },
+            { new(0), new(1000) },
+            { new(2000), new(2000) },
+            { new(3000), new(3000) },
+
+            { new(7), new(7) },
+            { new(7), new(1007) },
+            { new(2007), new(2007) },
+            { new(3007), new(3007) },
+
+            { new(22), new(22) },
+            { new(22), new(1022) },
+            { new(22), new(2022) },
+
+            { new(333), new(333) },
+            { new(333), new(1333) },
+            { new(333), new(2333) },
+            { new(333), new(3333) },
+        };
+    }
+
+    private static TheoryData<CarNumber, CarNumber> GetInequalityTestData()
+    {
+        return new TheoryData<CarNumber, CarNumber>()
+        {
+            { new(0), new(1) },
+            { new(1), new(2) },
+
+            { new(0), new(2000) },
+            { new(0), new(3000) },
+            { new(2000), new(3000) },
+
+            { new(7), new(2007) },
+            { new(7), new(3007) },
+
+            { new(22), new(3022) }
+        };
+    }
+}
